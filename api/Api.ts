0720621c6aacf8cea972044daf0c8ddc8a421134
peshkes <peshkesv@gmail.com>
2023-12-09 15:47:34 +0200

    Added mock API

---
+++ api/Api.ts
@@ -0,0 +1,26 @@
+
+type Role = 'admin' | 'employee';
+interface Answer {
+    name: string,
+    role: Role
+}
+
+interface User extends Answer{
+    id: string,
+    login: string,
+    password: string,
+}
+
+export class Api {
+    //mock data
+    private dataArray: User[] = [
+        {id: '1sad-sdaw2-faef4', login: 'admin', password: '7761', name: 'Valeriy', role: "admin"},
+        {id: '4fha-s24d-asd9', login: 'boris', password: '2705', name: 'Boris', role: "admin"},
+        {id: 'aglp2-aaaw5-98s9', login: 'employee', password: '1234', name: 'Boris', role: "admin"},
+    ]
+    logIn = async (login: string, password: string) => {
+        const ui = this.dataArray.findIndex(item => item.login === login);
+        if (this.dataArray[ui].login === login && this.dataArray[ui].password === password)
+            return {name: this.dataArray[ui].name, role: this.dataArray[ui].role}
+    }
+}
